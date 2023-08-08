e61de10dca21b97eae70ff275868067716785fc5
maximbtw <traglodet2001@gmail.com>
2023-08-08 22:05:17 +0300

    add checkBox scripts

---
+++ Assets/Scripts/UI/CheckBox/CheckBoxSliderAnimator.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UI.CheckBox
+{
+    public class CheckBoxSliderAnimator : UIBehaviour
+    {
+        [SerializeField] private Transform targetObject;
+        [SerializeField] private Vector2 normalPosition;
+        [SerializeField] private Vector2 selectedPosition;
+
+        [SerializeField] [Range(0.005f, 0.05f)]
+        private float speed;
+
+        private bool _selected;
+
+        protected override void Awake()
+        {
+            if (targetObject == null)
+            {
+                targetObject = gameObject.transform;
+            }
+        }
+
+        private void Update()
+        {
+            Vector2 currentPosition = targetObject.localPosition;
+            var rightPosition = _selected ? selectedPosition : normalPosition;
+
+            if (!currentPosition.Equals(rightPosition))
+            {
+                targetObject.localPosition = Vector2.Lerp(currentPosition, rightPosition, speed);
+            }
+        }
+
+        public void PlayAnimation(bool selected)
+        {
+            _selected = selected;
+
+#if UNITY_EDITOR
+            targetObject.localPosition = selected ? selectedPosition : normalPosition;
+#endif
+        }
+    }
+}
